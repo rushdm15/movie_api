5ed123a839772e65cd5f91ff251b4105428d070d
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-25 16:09:36 +0200

    added get requests

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const express = require("express");
 const app = express();
+// GET requests
+app.get("/", (req, res) => {
+  res.send("Welcome to my movie club!");
+});
+
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: __dirname });
+});
+
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
+// listen for requests
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
