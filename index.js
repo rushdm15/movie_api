eb460f5a7dde66b2b27812e79a0db384d02a9160
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-28 15:29:14 +0200

    added express code to route all of the endpoints

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 morgan = require("morgan");
+(bodyParser = require("body-parser")), (uuid = require("uuid"));
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(morgan("common"));
 app.use(express.static("public"));
 app.use((err, req, res, next) => {
@@ -69,4 +71,102 @@ app.get("/movies", (req, res) => {
 // listen for requests
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
+});
+
+// Gets the list of data about ALL movies
+
+app.get("/movies", (req, res) => {
+  res.json(students);
+});
+// Gets the data about a single movie, by title
+
+app.get("/movies/:title", (req, res) => {
+  res.json(
+    movies.find(movie => {
+      return movie.name === req.params.name;
+    })
+  );
+});
+
+// Adds data for a new movie to our list of favorites.
+app.post("/movies", (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.name) {
+    const message = "Missing name in request body";
+    res.status(400).send(message);
+  } else {
+    newStudent.id = uuid.v4();
+    movies.push(newMovie);
+    res.status(201).send(newMovies);
+  }
+});
+
+// Deletes a movie from our list of favorites
+app.delete("/movies/:id", (req, res) => {
+  let movie = movies.find(movie => {
+    return movie.id === req.params.id;
+  });
+
+  if (movie) {
+    movies = movies.filter(obj => {
+      return obj.id !== req.params.id;
+    });
+    res.status(201).send("Movie " + req.params.id + " was deleted.");
+  }
+});
+
+// Update the "user info" of a user by  username
+app.put("/movies/:name/:class/:grade", (req, res) => {
+  let movie = movies.find(movie => {
+    return movie.name === req.params.name;
+  });
+
+  if (movie) {
+    movie.classes[req.params.class] = parseInt(req.params.grade);
+    res
+      .status(201)
+      .send(
+        "Movie " +
+          req.params.name +
+          " was assigned a grade of " +
+          req.params.grade +
+          " in " +
+          req.params.class
+      );
+  } else {
+    res
+      .status(404)
+      .send("User with the username " + req.params.name + " was not found.");
+  }
+});
+
+// Gets the director of a movie
+app.get("/movies/:title/director", (req, res) => {
+  let movie = movies.find(movie => {
+    return movie.name === req.params.name;
+  });
+
+  if (movie) {
+    let movieDirector = Object.values(movie.director); // Object.values() filters out object's keys and keeps the values that are returned as a new array
+    // let sumOfGrades = 0;
+    // classesGrades.forEach(grade => {
+    //   sumOfGrades = sumOfGrades + grade;
+    // });
+
+    // let gpa = sumOfGrades / classesGrades.length;
+    // console.log(sumOfGrades);
+    // console.log(classesGrades.length);
+    // console.log(gpa);
+    // res.status(201).send("" + gpa);
+    // //res.status(201).send(gpa);
+  } else {
+    res
+      .status(404)
+      .send("Student with the name " + req.params.name + " was not found.");
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080");
 });
