57eee83f35986dbabf30f8dfaccc60d32f4f9b63
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-31 18:17:15 +0200

    commented GET requests

--- index.js
+++ index.js
@@ -58,17 +58,17 @@ let Movies = [
 ];
 
 // GET requests
-app.get("/", (req, res) => {
-  res.send("Welcome to my movie club!");
-});
-
-app.get("/documentation", (req, res) => {
-  res.sendFile("public/documentation.html", { root: __dirname });
-});
-
-app.get("/movies", (req, res) => {
-  res.json(topMovies);
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to my movie club!");
+// });
+//
+// app.get("/documentation", (req, res) => {
+//   res.sendFile("public/documentation.html", { root: __dirname });
+// });
+//
+// app.get("/movies", (req, res) => {
+//   res.json(topMovies);
+// });
 
 // // listen for requests
 // app.listen(8080, () => {
