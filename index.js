aea887ffb24ff7f93218aa65da60b5828867a2c9
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-31 20:10:00 +0200

    deleted const variables

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const uuid = require("uuid");
 
-const movies = index.movie;
-const users = index.user;
-
 const app = express();
 
 app.use(bodyParser.json());
