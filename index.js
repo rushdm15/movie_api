0850ed01009c496cce4e7c3f8f2f7efd9782415c
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-31 18:29:15 +0200

    un-commented variables, edited font

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const uuid = require("uuid");
 
+const movies = index.movie;
+const users = index.user;
 
 const app = express();
 
