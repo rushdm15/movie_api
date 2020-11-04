4d9da6389b696af263912b9296420f4d3a161546
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-04 15:08:09 +0200

    inserted movieSchema, mongoose.Schema

--- index.js
+++ index.js
@@ -196,3 +196,33 @@ app.get("/movies/:title/director", (req, res) => {
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080");
 });
+
+let movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+});
+
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
