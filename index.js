4118a825d447e5718cf77cc0943c811bc444268b
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-05 17:14:06 +0200

    GET request to get information on a specific user based on their username

--- index.js
+++ index.js
@@ -184,6 +184,18 @@ app.put("/users/:username", (req, res) => {
   });
 });
 
+// Get a user by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
 //   if (movie) {
 //     movie.classes[req.params.class] = parseInt(req.params.grade);
 //     res
