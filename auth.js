c460523d9895066d7881fa893e26203f4f4367bd
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-07 11:39:54 +0200

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,36 @@
+const jwtSecret = "your_jwt_secret"; //This has to be the same key used in the JWTStrategy
+
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
+
+require("./passport"); //Your local passport filter
+
+let generateJWTToken = user => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, //This is the username you're encoding in the JWTStrategy
+    expiresIn: "7d", //This specifies that the tokem will expire in 7 days
+    algorithm: "HS256" //This is the algorithm used to "sign" or encode the values of the JWTStrategy
+  });
+};
+
+/* POST login. */
+module.exports = (router) => {
+  router.post("/login"), (req, res) => {
+      passport.authenticate("local", { session: false },
+(error, user, info) => {
+          if (error || !user) {
+            return res.status(400).json({
+              message: "Something is not right",
+              user: user
+            });
+          }
+          req.login(user, { session: false }, (error) => {
+            if (error) {
+              res.send(error);
+            }
+            let token = generateJWTToken(user.toJSON());
+            return res.json({ user, token });
+          });
+        })(req, res);
+    });
+}
