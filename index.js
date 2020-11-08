25f886e5946a154f7d175ce9644e80804e51f658
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-08 15:03:04 +0200

    added hashedPassword

--- index.js
+++ index.js
@@ -146,9 +146,12 @@ app.get("/movies/:title", (req, res) => {
   Birthday: Date
 }*/
 app.post("/users", (req, res) => {
-  Users.findOne({ Username: req.body.Username })
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username })  // Search to see if a user
+  with the requested username already exists
     .then(user => {
       if (user) {
+        //If the user is found, send a response that it already exists
         return res.status(400).send(req.body.Username + "already exists");
       } else {
         Users.create({
