4c430b1a6c23ff2ce17918b87891f440905f4eca
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-09 20:34:44 +0200

    added new listening port

--- index.js
+++ index.js
@@ -346,4 +346,7 @@ app.delete(
 //   console.log("Your app is listening on port 8080");
 // });
 
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port " + port);
 });
