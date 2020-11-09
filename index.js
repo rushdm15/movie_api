c0e34bf708bccfe1cf509264a5f223b754dfd6df
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-09 18:31:02 +0200

    moved position, app.use cors

--- index.js
+++ index.js
@@ -10,23 +10,6 @@ const cors = require("cors");
 let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
 const { check, validationResult } = require("express-validator");
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn't found on the list of allowed allowedOrigins
-
-        let message =
-          "The CORS policy for this application doesn't allow acces from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
-
 const Movies = Models.Movie;
 const Users = Models.User;
 const app = express();
