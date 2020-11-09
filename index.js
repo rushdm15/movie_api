cfd7fa8b2b2b586ea373d3e0b42cbedcaf88df51
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-09 18:34:13 +0200

    moved position, app.use cors

--- index.js
+++ index.js
@@ -47,6 +47,23 @@ app.get("/", (req, res) => {
 //   console.log("Your app is listening on port 8080.");
 // });
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn't found on the list of allowed allowedOrigins
+
+        let message =
+          "The CORS policy for this application doesn't allow acces from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
+
 // Gets the list of data about ALL movies
 // JWT authentication to a specific endpoint
 app.get(
