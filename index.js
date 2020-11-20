f4b1fa9253af2342ecf4e9eb760e9205626f3978
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-20 12:59:07 +0200

    un-commented mongodb

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const app = express();
 
-// mongoose.connect("mongodb://localhost:27017/myFlixDB", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
