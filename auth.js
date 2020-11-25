a17edf3952cde38cbc78406d41de3e04991b66fd
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-25 19:01:55 +0200

    layout edits

--- auth.js
+++ auth.js
@@ -1,8 +1,6 @@
 const jwtSecret = "your_jwt_secret"; //This has to be the same key used in the JWTStrategy
-
 const jwt = require("jsonwebtoken"),
   passport = require("passport");
-
 require("./passport"); //Your local passport filter
 
 let generateJWTToken = user => {
@@ -16,20 +14,35 @@ let generateJWTToken = user => {
 /* POST login. */
 module.exports = router => {
   router.post("/login", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
-      if (error || !user) {
-        return res.status(400).json({
-          message: "Something is not right",
-          user: user
-        });
-      }
-      req.login(user, { session: false }, error => {
-        if (error) {
-          res.send(error);
+    passport.authenticate(
+      "local",
+      {
+        session: false
+      },
+      (error, user, info) => {
+        if (error || !user) {
+          return res.status(400).json({
+            message: "Something is not right",
+            user: user
+          });
         }
-        let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
-      });
-    })(req, res);
+        req.login(
+          user,
+          {
+            session: false
+          },
+          error => {
+            if (error) {
+              res.send(error);
+            }
+            let token = generateJWTToken(user.toJSON());
+            return res.json({
+              user,
+              token
+            });
+          }
+        );
+      }
+    )(req, res);
   });
 };
