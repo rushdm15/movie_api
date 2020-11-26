344aab145150949aa16905bc289af212bea7009c
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-26 17:10:09 +0200

    added JWT authentication

--- index.js
+++ index.js
@@ -201,16 +201,20 @@ app.delete(
 );
 
 // Get all users
-app.get("/users", (req, res) => {
-  Users.find()
-    .then(users => {
-      res.status(201).json(users);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.find()
+      .then(users => {
+        res.status(201).json(users);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Update a user's info, by username
 /* We’ll expect JSON in this format
