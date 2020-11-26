7469c72d3c1750f3228b94743694f7ce8220e430
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-26 17:09:33 +0200

    hashed password

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ app.put(
       {
         $set: {
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday
         }
