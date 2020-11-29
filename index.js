e49e317c828c121312594c1e3befd98dd81833cc
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-29 15:30:43 +0200

    edited post/users

--- index.js
+++ index.js
@@ -141,47 +141,34 @@ app.post(
       "Username",
       "Username contains non alphanumeric characters - not allowed."
     ).isAlphanumeric(),
-    check("Password", "Password is required")
-      .not()
-      .isEmpty(),
-    check("Email", "Email does not appear to be valid").isEmail()
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail(),
   ],
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
+  // passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     // check the validation object for errors
     let errors = validationResult(req);
-
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-
-    let hashedPassword = Users.hashPassword(req.body.Password);
-
-    Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
-      .then(user => {
-        if (user) {
-          //If the user is found, send a response that it already exists
-          return res.status(400).send(req.body.Username + "already exists");
-        } else {
-          Users.create({
-            Username: req.body.Username,
-            Password: hashedPassword,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-            .then(user => {
-              res.status(201).json(user);
-            })
-            .catch(error => {
-              console.error(error);
-              res.status(500).send("Error: " + error);
-            });
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send("Error: " + error);
+    try {
+      let hashedPassword = await bcrypt.hash(req.body.Password, 10);
+      let user = await Users.findOne({ Username: req.body.Username });
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists");
+      }
+      let usercreate = await Users.create({
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
       });
+      //https://flexnet91.herokuapp.com
+      return res.status(201).json(usercreate);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    }
   }
 );
 
