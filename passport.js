e52445d9d43632f1f8811bcbf1d33191a770b57e
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-30 21:02:25 +0200

    edited typo

--- passport.js
+++ passport.js
@@ -45,7 +45,7 @@ passport.use(
       secretOrKey: "your_jwt_secret"
     },
     (jwtPayload, callback) => {
-      return Users.findByID(jwtPayload._id)
+      return Users.findById(jwtPayload._id)
         .then(user => {
           return callback(null, user);
         })
