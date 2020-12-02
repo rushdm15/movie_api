dda96ff796bad39405d001c9f1d2057eed792266
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-02 14:28:30 +0200

    app.use(cors());

--- index.js
+++ index.js
@@ -36,27 +36,28 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
+app.use(
+  cors());
+// {
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn't found on the list of allowed allowedOrigins
+
+//         let message =
+//           "The CORS policy for this application doesn't allow acces from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
+
 // GET requests
 app.get("/", (req, res) => {
   res.send("Welcome to my movie club!");
 });
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn't found on the list of allowed allowedOrigins
-
-        let message =
-          "The CORS policy for this application doesn't allow acces from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
 
 // Gets the list of data about ALL movies
 app.get(
